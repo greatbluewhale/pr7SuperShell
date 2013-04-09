ad8cdeb81409782fc79593a43f8ef65482ff8516
nickdys <nwd5069@psu.edu>
2013-04-09 17:22:55 -0400

    Fixing compiler warnings
    part 3

--- pr7.2.c
+++ pr7.2.c
@@ -461,7 +461,7 @@ int cleanup_terminated_children(void)
     printf("process %d terminated with status %d", pid, status);
     entry = list_update_entry(&background_pid_table, pid, status);
     if (verbose) list_print(&background_pid_table);
-    if (entry != NULL) list_remove(&background_pid_table, &entry);
+    if (entry != NULL) list_remove(&background_pid_table, entry);
     count++;
   }
   
